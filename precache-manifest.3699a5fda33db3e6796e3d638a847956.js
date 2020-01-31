0b99fb964645719cb646cd916e8087512742f83a
sh0la <sholaadeniyi@gmail.com>
2020-01-31 09:08:43 -0600

    Updates

---
+++ precache-manifest.3699a5fda33db3e6796e3d638a847956.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5be2de20b6cd42e063c2b5bcabf432",
+    "url": "/chirp/index.html"
+  },
+  {
+    "revision": "042dfd9f069d264588e4",
+    "url": "/chirp/static/css/main.1d911f20.chunk.css"
+  },
+  {
+    "revision": "24e59dde6b2b76c40240",
+    "url": "/chirp/static/js/2.c1330d2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chirp/static/js/2.c1330d2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "042dfd9f069d264588e4",
+    "url": "/chirp/static/js/main.e2d91579.chunk.js"
+  },
+  {
+    "revision": "ae8c1123060e09bbc0dc",
+    "url": "/chirp/static/js/runtime-main.cb5525f0.js"
+  },
+  {
+    "revision": "c44a013163b3c13fd42ac4e383302e08",
+    "url": "/chirp/static/media/boy.c44a0131.jpg"
+  },
+  {
+    "revision": "a5bbcc98e2f415f4a5473ace84aac12b",
+    "url": "/chirp/static/media/cat.a5bbcc98.jpg"
+  },
+  {
+    "revision": "36f39706ef9c4c01a0c4efa9c9214d57",
+    "url": "/chirp/static/media/girl.36f39706.jpg"
+  }
+]);
