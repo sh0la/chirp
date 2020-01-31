74871fc636966291db181d2356b25e03ed593fce
sh0la <sholaadeniyi@gmail.com>
2020-01-30 23:46:27 -0600

    Update username props

--- src/Username.js
+++ src/Username.js
@@ -3,7 +3,7 @@ import React from 'react';
 function UserName(props) {
   console.log(props)
   return(
-    <p className="userName">{props.stringName}</p>
+    <p className="userName">{props.username}</p>
   )
 }
 
