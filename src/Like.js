9bdc493bd882010f1e73f414ca99316a6a85b741
sh0la <sholaadeniyi@gmail.com>
2020-01-30 23:48:46 -0600

    Add functionality for user likes

--- src/Like.js
+++ src/Like.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Like() {
-  return(
-    <p className="userName"></p>
-  )
+class Like extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    console.log(props.likes)
+  }
+  
+  handleClick() {
+    this.props.likeCounts()
+  }
+
+  render() {  
+    return (
+      <React.Fragment>
+        <div class="counter">
+          <span class="like" 
+            onClick={this.handleClick} 
+            counter={this.props.counter}
+            key={this.props.id} 
+          > Like
+          </span>
+            {this.props.likes}
+        </div>
+      </React.Fragment>
+    )
+  }
 }
+
 
 
 export default Like;
