ee61c04e3e1794440c93d11b6496f5c67e67926a
sh0la <sholaadeniyi@gmail.com>
2020-01-31 06:34:39 -0600

    Increase size of h1 tag

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,6 @@ function Header() {
   return(
     <header>
       <h1>Chirp</h1>
-      <nav>
-        <ul>
-          <li>Filter</li>
-          <li>Sort</li>
-        </ul>
-      </nav>
     </header>
   )
 }
