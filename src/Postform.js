19dfd42652ccf96135de04bbd9dac086f94eaa31
sh0la <sholaadeniyi@gmail.com>
2020-01-30 23:54:27 -0600

    Setup form for user input

--- src/Postform.js
+++ src/Postform.js
@@ -5,24 +5,31 @@ class Postform extends Component {
   constructor() {
     super();
 
+    //receive ur array of users, as props (this.props.array)
+
     this.state = {output:''};
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleSubmit(event) {
+    console.log(this.props.idStream)
+    const text = this.state.output;
     event.preventDefault()
-    
+    this.props.formSubmit(text)
+
   }
 
   handleChange(event) {
+
+    //u will  make an object id,john,avatar, message
+    //push this object into ur array usersArray.push(ur new object)
     this.setState({output: event.target.value})
   }
 
 
   render() {
-    const {output} = this.state
-    console.log(output)
+
     return(
       <form 
         action=""
@@ -31,12 +38,15 @@ class Postform extends Component {
         <input 
           type="text" 
           placeholder="What's on your mind..." 
-          value={output} 
+          className='message'
+          value={this.state.output} 
           onChange={this.handleChange} 
-          
-               
-               
+          idComment={this.props.idStream}        
         />
+        <input 
+          className="btn"
+          type="button" 
+          value="Submit"/>
       </form>
     )
   }
