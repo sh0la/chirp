77a0c378c4c6b172732ba7ac5dc1b37294634b26
sh0la <sholaadeniyi@gmail.com>
2020-01-30 23:47:09 -0600

    Update user image props

--- src/Avatar.js
+++ src/Avatar.js
@@ -3,10 +3,12 @@ import React from 'react';
 function Avatar(props) {
   return(
     <div className="userImg">
-      <span>Submitted by</span>
+      
       <img //src={require("./images/girl.jpg")} 
-           src={props.avatarUrl}
-           alt=""/>
+           src={props.avatar}
+           alt=""
+           className='userImg'
+      />
     </div>
   )
 }
