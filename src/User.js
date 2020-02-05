0e04f5436fd0c71f5e0326cf24d1959bbe5717eb
sh0la <sholaadeniyi@gmail.com>
2020-02-04 22:42:50 -0600

    Add dropdown for users

--- src/User.js
+++ src/User.js
@@ -11,18 +11,30 @@ class User extends Component {
     this.state = {
       users: [
         {
-          id:1,
+        id:1,
         name: 'Paul',
         avatarUrl: './images/boy.jpg',
         
       },
-       {id:2,
+       {
+        id:2,
         name: 'Koby',
         avatarUrl: './images/cat.jpg'
       },
-      {id:3,
+      {
+        id:3,
         name: 'Lobs',
         avatarUrl: './images/girl.jpg'
+      },
+      {
+        id:4,
+        name: 'Theresa',
+        avatarUrl: './images/cat.jpg'
+      },
+      {
+        id:5,
+        name: 'Quinn',
+        avatarUrl: './images/boy.jpg'
       }
       ],
     }
@@ -44,11 +56,37 @@ class User extends Component {
         )
       })
 
-    return( <div>{userList}
-    <Page userArray ={this.state.users} />
+    // return( 
+    //   <div>
+    //     {userList}
+    //     <Page userArray ={this.state.users} />
+    //   </div>
+    // )
+    return(
+      <div>
+        <p>Select a user: </p>
+        <select name="" id="">
+          <option value="Paul">
+            Paul
+          </option>
+          <option value="Paul">
+            Lobs
+          </option>
+          <option value="Koby">
+            Koby
+          </option>
+          <option value="Theresa">
+            Theresa
+          </option>
+          <option value="Quinn">
+            Quinn
+          </option>
+        </select>
       </div>
+      
     )
   }
 }
 
 export default User;
+
